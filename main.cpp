6820decf78e27277bf45bbcc287ce6c620131fd5
nad12 <mgardasaib@umail.utm.ac.mu>
2019-07-28 17:21:48 +0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /* displaying text
+    *cout << "Hello world!" << endl;
+    *cout << "Hello world! \n";
+    *cout << "Hello World two";
+    */
+
+    /*Variable declaration
+    *int a = 4;
+    *int b = 24;
+
+    *int sum = a + b;
+    *cout << sum;
+    */
+
+    return 0;
+}
