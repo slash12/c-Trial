d85fb56ddb0f9892ad7fd2b05b6771cd2cb1e67c
nad12 <mgardasaib@umail.utm.ac.mu>
2019-07-28 18:12:02 +0400

    1.03.09 arrived

--- main.cpp
+++ main.cpp
@@ -2,35 +2,17 @@
 
 using namespace std;
 
+//function prototyping
+void printSomething();
+
 int main()
 {
-    /* displaying text
-    *cout << "Hello world!" << endl;
-    *cout << "Hello world! \n";
-    *cout << "Hello World two";
-    */
-
-    /*Variable declaration
-    *int a = 4;
-    *int b = 24;
-
-    *int sum = a + b;
-    *cout << sum;
-    */
-
-    //creating variable without entering their value
-    int a;
-    int b;
-    int sum;
-
-    cout << "Enter a number! \n";
-    cin >> a;
-
-    cout << "Enter another number \n";
-    cin >> b;
-
-    sum = a + b;
-    cout << "The sum is " << sum << endl;
+    printSomething();
 
     return 0;
+}
+
+void printSomething()
+{
+    cout << "something" << endl;
 }
